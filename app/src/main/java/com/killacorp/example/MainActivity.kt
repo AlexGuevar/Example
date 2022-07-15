bbb988c57539d79935d2f0e5a307fce0ad2fe80a
gagmn <gagm.nv@gmail.com>
2022-07-15 12:45:00 -0500

    navegación entre actividades

--- app/src/main/java/com/killacorp/example/MainActivity.kt
+++ app/src/main/java/com/killacorp/example/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.killacorp.example
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.killacorp.example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var layout: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        layout = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-       //first commit
+        setContentView(layout.root)
+        layout.button.setOnClickListener {
+            startActivity(
+                Intent(this,HomeActivity::class.java)
+            )
+        }
     }
 }
